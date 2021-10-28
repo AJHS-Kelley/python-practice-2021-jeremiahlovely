7cf8bc8ddedf23c61c70dc7f4f320c9c5c54e694
Jeremiah L <90405735+jeremiahlovely@users.noreply.github.com>
2021-10-28 12:05:52 -0400

    Updated variables_practice.py
    added string, int, float

--- variables_practice.py
+++ variables_practice.py
@@ -1,1 +1,11 @@
 # variables Practice, jeremiah lovely, 10/28/21 11:40, version 0.0
+
+# Variable Types -- Most Common
+
+my_float = 1.5
+my_int = 5
+my_string = "I like dragons."
+
+print(my_float)
+print(my_int)
+print(my_string)
